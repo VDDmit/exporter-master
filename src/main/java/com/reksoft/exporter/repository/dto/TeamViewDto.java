c2bf809bfad7833614c22ace5f3f8611b243abf8
Даниил Воропаев <114810370+VDDmit@users.noreply.github.com>
2025-07-07 00:11:05 +0300

    Add unit tests (#5)
    * update-5 add mapstruct mappers
    
    * update-6 add containerization
    
    * update-6 add containerization
    
    * update-6 add JUnit+Mockito test

--- src/main/java/com/reksoft/exporter/repository/dto/TeamViewDto.java
+++ src/main/java/com/reksoft/exporter/repository/dto/TeamViewDto.java
@@ -1,12 +1,14 @@
 package com.reksoft.exporter.repository.dto;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
 @Data
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TeamViewDto {
     Integer id;
